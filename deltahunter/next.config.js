f59d503326e6c78047aa1fae75f465a314c413c1
jesu-m0 <jesusmd2000@gmail.com>
2026-03-07 01:39:25 +0100

    Update next.config.js

--- deltahunter/next.config.js
+++ deltahunter/next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites: async () => [
-    {
-      source: "/api/analyze",
-      destination: "http://localhost:5328/api/analyze",
-    },
-  ],
+  rewrites: async () => {
+    if (process.env.NODE_ENV === "development") {
+      return [
+        {
+          source: "/api/analyze",
+          destination: "http://localhost:5328/api/analyze",
+        },
+      ];
+    }
+    return [];
+  },
 };
 
 module.exports = nextConfig;
